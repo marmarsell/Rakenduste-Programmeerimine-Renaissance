032a151ba10a07f0d78702a8a6724c5fb1783e6f
marmarsell <marsell@tlu.ee>
2025-10-28 22:07:41 +0200

    feat: added capability to create new flashcards, though only debug. Integration with localstorage

--- flashcard/flashcard-madness/src/App.tsx
+++ flashcard/flashcard-madness/src/App.tsx
@@ -2,12 +2,44 @@
 import './App.css'
 
 function App() {
+
+  type Flashcard = {
+    que: string,
+    v1: string,
+    v2: string,
+    v3: string
+  }
+
+  //localstorage initialisation
+  if(!localStorage.getItem("flashcard")) {
+    const nuevaStorage = [] as Array<Flashcard>
+    localStorage.setItem("flashcard", JSON.stringify(nuevaStorage))
+  }
+
   //const [count, setCount] = useState(0)
+
+  function thyCardCreateth() {
+    const testFlashcard = {
+      que: "is it a test?",
+      v1: "yes",
+      v2: "no",
+      v3: "unsure"
+    } as Flashcard
+
+    console.log("adding Test Flashcard");
+
+    const saved = localStorage.getItem("flashcard")
+    const tempFlashcardStorage = JSON.parse(saved as string) as Array<Flashcard>
+    tempFlashcardStorage.push(testFlashcard)
+    localStorage.setItem("flashcard", JSON.stringify(tempFlashcardStorage))
+
+    console.log(tempFlashcardStorage)
+  }
 
   return (
     <>
       <div>
-        <div>flashcardddd</div>
+        <div>flashcardddd <button onClick={() => thyCardCreateth()}>wawaa</button></div>
       </div>
     </>
   )
